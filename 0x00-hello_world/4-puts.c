ec63d55edd461639789b2eb41838470ce099e123
Eznira <ihimarinze@gmail.com>
2023-06-16 10:49:47 -0700

    added script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- *Main - entry point
- *Description: to print ""Programmming is like building a multillingual puzzle"
- *Returns: 0
-*/
+ * main - Entry point of the program
+ *
+ * description: to print ""Programmming is like building.."
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

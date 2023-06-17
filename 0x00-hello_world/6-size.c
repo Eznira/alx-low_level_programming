b7c79dcc8b5f1265cb9691a31d13d8512c588227
Eznira <ihimarinze@gmail.com>
2023-06-17 08:46:05 -0700

    adde 6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/*
+* main - Entry point
+* Return : 0 on success
+* Description :  prints the size of various types on the computer it is compiled and run
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
+	return (0);
+}

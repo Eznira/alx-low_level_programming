64e4b511ccad42fe4883d55625920e95ded6ffee
Eznira <ihimarinze@gmail.com>
2023-06-18 08:04:54 -0700

    added 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,23 @@
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main - Entry point of the program
  *
- * Description: Prints the message "and that piece of art is useful" -
- *              Dora Korpar, 2015-10-19" to the standard error.
+ * Description: Prints "and that piece of art is useful" -
+ *      Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
  * Return: Always 1 (indicating failure)
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - "
-"Dora Korpar, 2015-10-19\n";
+	char message_part1[] = "and that piece of art is useful\" - ";
+	char message_part2[] = "Dora Korpar, 2015-10-19\n";
+	char message[60];
 
-	write(2, message, 59);
+	strcpy(message, message_part1);
+	strcat(message, message_part2);
+	write(2, message, strlen(message));
 
 	return (1);
 }

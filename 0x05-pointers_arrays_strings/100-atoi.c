f1551ae328be37cbf7e9f13df542bcecf73c83c1
Eznira <ihimarinze@gmail.com>
2023-06-27 09:28:36 -0700

    added 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,8 +9,8 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int i = 0;
+	int digit;
 
-	/* Check for the sign */
 	if (s[0] == '-')
 	{
 		sign = -1;
@@ -21,20 +21,29 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* Convert the string to an integer */
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + (s[i] - '0');
+			digit = s[i] - '0';
+
+			if (result > INT_MAX / 10 ||
+			    (result == INT_MAX / 10 && digit > INT_MAX % 10))
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
+			result = result * 10 + (sign * digit);
 		}
 		else
 		{
-			/* Stop conversion if a non-digit char is encountered */
 			break;
 		}
 		i++;
 	}
 
-	return (sign * result);
+	return (result);
 }

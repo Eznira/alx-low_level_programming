1963d85e02fc258dd984e2b41f0011d092f8dd60
Eznira <ihimarinze@gmail.com>
2023-06-29 14:31:08 -0700

    updated main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-int _atoi(char *s);
-void puts2(char *str);
-void puts_half(char *str);
+char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */

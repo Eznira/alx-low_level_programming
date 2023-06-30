aa9fdf3bbc941aeecce7c7077a26ff7805909976
Eznira <ihimarinze@gmail.com>
2023-06-30 11:25:49 -0700

    added 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+/**
+ * reverse_array - Reverses the elements of an array
+ * @a: The integer array
+ * @n: Number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int mid_index;
+	int n_element;
+	int i;
+	int temp;
+
+	mid_index = n / 2;
+
+	for (i = 0; i < mid_index; i++)
+	{
+		n_element = n - i;
+
+		temp = *(a + n_element);
+		*(a + n_element) = *(a + i);
+		*(a + i) = *temp;
+	}
+}

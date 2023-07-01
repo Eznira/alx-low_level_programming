931aab02dfcbafa421b613f6977189ab1e566b0e
Eznira <ihimarinze@gmail.com>
2023-06-30 21:01:59 -0700

    added 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,33 @@
  */
 void print_number(int n)
 {
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	int divisor = 1;
+	int temp = n;
 
-	_putchar('0' + (n % 10));
+	while (temp / 10 != 0)
+	{
+		divisor *= 10;
+		temp /= 10;
+	}
+
+	while (divisor != 0)
+	{
+		int digit = n / divisor;
+
+		_putchar('0' + digit);
+		n %= divisor;
+		divisor /= 10;
+	}
 }

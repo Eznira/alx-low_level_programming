ed336737ad5acc76bf32184284ffae80da47919e
Eznira <ihimarinze@gmail.com>
2023-06-30 21:12:12 -0700

    added 102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the value of a[2] as 98
+ * without using the variable 'a' and modifying 'p'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+
+	/* ...so that this prints "a[2] = 98" */
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}

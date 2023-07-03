19e5d78c1e8c98e6c175c39d43c0561b4d7be128
Eznira <ihimarinze@gmail.com>
2023-07-03 11:00:58 -0700

    added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - Locates the first occurrence of a character in a string
+ * @s: Pointer to the string to search
+ * @c: Character to locate
+ *
+ * Return: Pointer to the first occurrence of @c in @s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	return (NULL);
+}

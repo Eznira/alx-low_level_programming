f39f962f04ab39d00522d56b7ce7376d1a28412e
Eznira <ihimarinze@gmail.com>
2023-07-05 17:51:23 -0700

    added 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - Returns the factorial of a number.
+ * @n: The number.
+ *
+ * Return: The factorial of the number.
+ *         -1 if n is negative (error).
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return -1;
+
+	if (n == 0)
+		return 1;
+
+	return n * factorial(n - 1);
+}

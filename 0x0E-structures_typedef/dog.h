93e7bd8ac91fff328aab59cb24f7cf5525a39861
Eznira <ihimarinze@gmail.com>
2023-07-18 16:45:59 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - contains info about dog object
+ *
+ * @name: The name of the dog
+ * @age: The dog's name
+ * @owner: Owner's name.
+ *
+ * Description - A dog struct conatianing info about the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}

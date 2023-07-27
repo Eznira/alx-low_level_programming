50896d9867beecb97575038c3b1a7e769eb02238
Eznira <ihimarinze@gmail.com>
2023-07-27 14:07:10 -0700

    added 0-print_list.c

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/**
+ * struct node - Node object for the singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: address of the next node
+ *
+ * Description: custom singly linked list structure
+ */
+typedef struct node
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif /* MAIN_H */

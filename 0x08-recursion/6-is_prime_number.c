16555353598bcc3a5e8ad3ad2996902c5dd83562
Eznira <ihimarinze@gmail.com>
2023-07-29 11:10:26 -0700

    addede 5..

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,11 +6,13 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 		return (0);
 
 	/* Check for factors from 2 up to the square root of n */
-	for (int i = 2; i * i <= n; i++)
+	for (i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 			return (0);

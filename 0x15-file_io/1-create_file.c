df9b6ef6410567c1ca22d38e28d03a38d516f51b
Eznira <ihimarinze@gmail.com>
2023-08-08 10:28:39 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <string.h>
 /**
  * create_file - Creates a file and writes text content to it.
  * @filename: Name of the file to create.
@@ -13,7 +13,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor, bytes_written;
-	mode_t mode = S_IRUSR | S_IWUSR;
+	mode_t mode;
+
+	mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);

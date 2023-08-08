bc9f7b3bff87bd2fd903ba4a4a38c894dba8f75f
Eznira <ihimarinze@gmail.com>
2023-08-08 09:44:20 -0700

    added 2 project files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - Creates a file and writes text content to it.
+ * @filename: Name of the file to create.
+ * @text_content: NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	int file_descriptor, bytes_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(file_descriptor, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}

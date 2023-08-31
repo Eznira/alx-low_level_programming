704dfeed076ed49152ab635bf53ec74bddea7ee0
Ihim Arinze <ihimarinze@gmail.com && git config --global user.name Ihim>
2023-08-30 18:55:07 -0700

    new file: ,1-print_rev_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,10 @@
 #include <unistd.h>
 
 void _putchar(char c);
+void _puts_recursion(char *a);
+void _puts_rev_recursion(char *a);
+int _strlen_recursion(char *str);
+int factorial(int num);
+int pow_recursion(int base, int pow);
 
 #endif /* MAIN_H */

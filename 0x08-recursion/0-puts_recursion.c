353b0db98768a9b7c92d3637195f5abcce5fad0e
Ihim Arinze <ihimarinze@gmail.com && git config --global user.name Ihim>
2023-08-30 18:32:51 -0700

    added new folder: 0x08..77djd

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - Prints out a string to the standard output,
+ *                   recurcively.
+ * @a: The pointer to the string.
+ */
+void _puts_recursion(char *a)
+{
+	if (*a == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*a);
+	_puts_recursion(a + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This folder contains practise code onnrecursion.

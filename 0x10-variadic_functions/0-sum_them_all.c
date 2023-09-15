d90a2b6ff3e1a44471783976ca1250e9f1108547
Ihim Arinze <ihimarinze@gmail.com && git config --global user.name Ihim>
2023-09-14 20:25:59 -0700

    added new folder: 0x10..

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: The number of arguments.
+ * @...: Variable-length arguments (n arguments).
+ *
+ * Return: The sum of all parameters. If n == 0, return 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void  _putchar(char *a);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* MAIN_H */
